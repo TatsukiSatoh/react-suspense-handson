2804353871d4f373b2efa200c196a7c58bef851c
ju12yw <76781368+ju12yw@users.noreply.github.com>
2024-06-08 14:32:21 +0900

    try suspence

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { Suspense } from 'react'
 import './App.css'
+import { AlwaysSuspend } from "./components/AlwaysSuspend"
 
 function App() {
   return (
     <div className="text-center">
       <h1 className="text-2xl">React App!</h1>
+      <Suspense fallback={<div>loading...</div>}>
+        <AlwaysSuspend />
+      </Suspense>
+      
     </div>
   )
 }
