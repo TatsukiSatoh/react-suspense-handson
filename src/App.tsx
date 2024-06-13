25c0504b60aa04fc46986468bf9597e897910253
ju12yw <76781368+ju12yw@users.noreply.github.com>
2024-06-14 08:57:54 +0900

    feat: complete chapter 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import { Suspense } from 'react'
+import { useState, Suspense } from 'react'
 import './App.css'
-import { AlwaysSuspend } from "./components/AlwaysSuspend"
+import { AlwaysSuspend, SometimeSuspend } from "./components/AlwaysSuspend"
+import { RenderingNotifier } from "./components/RenderingNotifier"
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [count1, setCount1] = useState(0)
   return (
     <div className="text-center">
       <h1 className="text-2xl">React App!</h1>
+      <RenderingNotifier name='outside-Suspense' />
       <Suspense fallback={<div>loading...</div>}>
-        <AlwaysSuspend />
+        {/* <AlwaysSuspend /> */}
+        <SometimeSuspend />
+        <RenderingNotifier name='inside-Suspense' />
+        <button className='border p-1' onClick={() => setCount((c) => c + 1)}>
+          {count}
+        </button>
       </Suspense>
-      
     </div>
   )
 }
--- src/components/AlwaysSuspend.tsx
+++ src/components/AlwaysSuspend.tsx
@@ -1,7 +1,18 @@
+import React from "react"
+
 function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms))
 }
 
 export const AlwaysSuspend: React.FC = () => {
+    console.log("AlwaysSuspend: suspending...")
     throw sleep(1000)
+}
+
+export const SometimeSuspend: React.FC = () => {
+    if(Math.random () < 0.5){
+        throw sleep(1000)
+    }
+
+    return <p>Hello World!</p>
 }
