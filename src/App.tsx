e91f4b1eed6b4f396f7eba9bd92be167b17e29da
ju12yw <76781368+ju12yw@users.noreply.github.com>
2024-06-14 09:42:14 +0900

    complete chapter 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import { useState, Suspense } from 'react'
 import './App.css'
-import { AlwaysSuspend, SometimeSuspend } from "./components/AlwaysSuspend"
+import { AlwaysSuspend, SometimeSuspend, DataLoader } from "./components/AlwaysSuspend"
 import { RenderingNotifier } from "./components/RenderingNotifier"
 
 function App() {
   const [count, setCount] = useState(0)
   const [count1, setCount1] = useState(0)
+
   return (
     <div className="text-center">
       <h1 className="text-2xl">React App!</h1>
-      <RenderingNotifier name='outside-Suspense' />
+      <Suspense fallback={<p>Loading...</p>}>
+        <DataLoader />
+      </Suspense>
+      {/* <RenderingNotifier name='outside-Suspense' />
       <Suspense fallback={<div>loading...</div>}>
-        {/* <AlwaysSuspend /> */}
         <SometimeSuspend />
         <RenderingNotifier name='inside-Suspense' />
         <button className='border p-1' onClick={() => setCount((c) => c + 1)}>
           {count}
         </button>
-      </Suspense>
+      </Suspense> */}
     </div>
   )
 }
--- src/components/AlwaysSuspend.tsx
+++ src/components/AlwaysSuspend.tsx
@@ -1,7 +1,33 @@
-import React from "react"
+import React, {useState} from "react"
 
 function sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function fetchData1(): Promise<string> {
+    await sleep(1000)
+    
+    return `Hello, ${(Math.random() * 1000).toFixed(0)}`
+}
+
+
+let data: string | undefined
+const dataMap: Map<string, string> = new Map()
+
+// カスタムHooks(勉強用のためファイル分割なし)
+function useData1(cacheKey: string): string {
+    const cachedData = dataMap.get(cacheKey)
+    if (cachedData === undefined){
+        throw fetchData1().then((d) => dataMap.set(cacheKey, d))
+    }
+    return cachedData
+}
+
+export const DataLoader: React.VFC = () => {
+    const data = useData1("DataLoader1")
+
+    // データがあればそれを表示
+    return <div>data is {data}</div>
 }
 
 export const AlwaysSuspend: React.FC = () => {
